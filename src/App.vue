94247d968ae4970a3c1f1b238551c90244752259
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2025-05-04 21:57:18 +0600

    learning how to create components and place it

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+// import TestComponent from './components/TestComponent.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,7 +9,8 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Well Done, Nafis" />
+      <!-- <TestComponent name="Nafis" /> -->
     </div>
   </header>
 
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <h2>Hey your first component {{ name }}</h2>
+</template>
