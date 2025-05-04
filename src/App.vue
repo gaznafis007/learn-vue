9a2a6f5a54d608778f19d1a5bfa39cb003cb86f7
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2025-05-04 22:25:13 +0600

    exploring options api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Gazi Nafis Md Abdullah',
+      status: true,
+      tasks: ['task One', 'task Two', 'task Three'],
+      link: 'https://gazi-nafis-rafi.vercel.app/',
+    }
+  },
+  methods: {
+    toggleStatus() {
+      this.status = !this.status
+    },
+  },
+}
+</script>
+
 <template>
-  <h2>Hello Vue, I here to crack you</h2>
+  <h1>Hey, My name is {{ name }}</h1>
+  <br />
+  <p v-if="status">User is active</p>
+  <p v-else>User is inactive</p>
+  <a v-bind:href="link">visit my portfolio</a>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <button @click="toggleStatus">change user status</button>
 </template>
